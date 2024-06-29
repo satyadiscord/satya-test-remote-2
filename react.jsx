5c88dab97509bc14f07e5ca8ca0e5321b780022a
satyadiscord <satyadiscord@gmail.com>
2024-06-30 01:02:55 +0800

    menambahkan listNamaUser pada file react.jsx dengan cara di looping menggunakan method map

--- react.jsx
+++ react.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { useState } from "react";
 
+const listNamaUser = ["sanjaya", "doyok", "dodi", "aprilla"];
 function App() {
   const [nameUser, setNameUser] = useState("Broday");
+  const [listDataUser, setListDataUser] = useState(listNamaUser);
+
   return (
     <>
       <h1>Hello, This React + GIT Remote</h1>
       <h1>{nameUser}</h1>
+      <ul>
+        {listDataUser.map((userNamee, index) => (
+          <li key={index}>
+            <a href="#">{userNamee}</a>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }

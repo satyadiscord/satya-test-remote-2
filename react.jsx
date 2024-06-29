f0cfd851c00592d6623ac0fefd49531b9258c6d1
satyadiscord <satyadiscord@gmail.com>
2024-06-30 00:49:10 +0800

    menambahkan name user pada file react.jsx menggunakan useState Hook

--- react.jsx
+++ react.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
+  const [nameUser, setNameUser] = useState("Broday");
   return (
     <>
       <h1>Hello, This React + GIT Remote</h1>
+      <h1>{nameUser}</h1>
     </>
   );
 }
